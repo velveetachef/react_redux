42374c629a8f66eada8f4a209535b60355c5b37f
velveetachef <jeremysabat@gmail.com>
2020-09-09 20:21:38 -0700

    seasons

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props); // always
+    state = { lat: null, errorMessage: ''};
 
-        // ONLY TIME YOU DIRECT ASSIGN STATE!!!!
-        this.state = { lat: null };
-
+    componentDidMount() {
+        console.log('component rendered to the screen');
         window.navigator.geolocation.getCurrentPosition(
-            (position) => {
-                // setState !!!!!!!!!!
-                this.setState({ lat: position.coords.latitude });
-            },
-            (err) => console.log(err)
+            (position) =>this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
         )
     }
 
-    // React says this must be defined
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+
+        return <Spinner message="Please accept location request"/>
+    }
+
     render() {
-        return <div>Latitiue: {this.state.lat}</div>
+        return <div className="border red">
+            {this.renderContent()}
+        </div>
     }
 }
 
